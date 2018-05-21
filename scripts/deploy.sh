bff302e780b2e738001e30657d849433f0b4d3b7
mariojmdavid <mariojmdavid@gmail.com>
2018-05-21 14:20:01 +0100

    add ssm conf file, update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-#IMAGE="mariojmdavid/caso"
-IMAGE="caso"
+IMAGE="mariojmdavid/caso"
 CONT="caso"
+
+mkdir -p /etc/caso \
+         /etc/apel \
+         /var/spool/caso \
+         /var/spool/apel/outgoing/openstack \
+         /var/log/caso \
+         /var/log/apel
+
+cp run-caso.sh run-ssmsend.sh /usr/local/bin/
+cp voms.json caso.conf /etc/caso/
+cp sender.cfg /etc/apel/
+
+CACERT="/etc/certs/caroot.pem"
+CERT="/etc/grid-security/apelcert.pem"
+KEY="/etc/grid-security/apelkey.pem"
+
+CONT_VOLS="-v /etc/caso:/etc/caso \
+           -v /etc/apel:/etc/apel \
+           -v /var/spool/caso:/var/spool/caso \
+           -v /var/spool/apel/outgoing/openstack:/var/spool/apel/outgoing/openstack \
+           -v /var/log/caso:/var/log/caso \
+           -v /var/log/apel:/var/log/apel \
+           -v ${CACERT}:${CACERT} \
+           -v ${CERT}:${CERT} \
+           -v ${KEY}:${KEY}"
 
 if  [ -z $(docker images -q ${IMAGE}) ]
 then
@@ -10,15 +34,12 @@ then
   docker pull ${IMAGE}
 fi
 
-
-
 if [ ! "$(docker ps -a | grep ${CONT})" ]
 then
   echo "Creating container"
-  docker run --name ${CONT} ${IMAGE}
+  docker run --name ${CONT} ${CONT_VOLS} ${IMAGE}
 fi
 
-cp run-caso.sh run-ssmsend.sh /usr/local/bin/
 cp caso.cron /etc/cron.d/caso
 cp ssmsend.cron /etc/cron.d/ssmsend
 
